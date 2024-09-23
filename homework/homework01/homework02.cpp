5fde7e4b218531af0fd6c3dc7188db708599f49c
rlatjdgnsdl2 <rlatjdgnsdl2@gmail.com>
2024-09-23 17:56:59 +0900

    NumberCount NumberToString

---
+++ homework/homework01/homework02.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+int NumberCount(int _value)
+{
+	int result = 0;
+	int devisionNum = 1;
+
+
+
+	while (true)
+	{
+		
+	}
+}
+
+int main()
+{
+	int aa = 1;
+	int bb = aa % 10;
+	int a = 0;
+}
