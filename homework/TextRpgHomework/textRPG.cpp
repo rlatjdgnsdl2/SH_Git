bb4c5844ddc43962c94ff15364b2be3ec13ecd7d
rlatjdgnsdl2 <rlatjdgnsdl2@gmail.com>
2024-09-24 14:24:24 +0900

    textRPG

--- homework/TextRpgHomework/textRPG.cpp
+++ homework/TextRpgHomework/textRPG.cpp
@@ -10,6 +10,9 @@ const int NAMELEN = 10;
 // 절차지향 
 // 어떠한 현실적인 개념을 컴퓨터 세계로 끌고 들어온다.
 // 상태와 
+
+char UIName[NAMELEN] = "NONE";
+
 int PlayerAtt = 0;
 int PlayerHp = 0;
 char PlayerName[NAMELEN] = "NONE";
@@ -40,27 +43,31 @@ void StrCopy(char* _Arr, int _BufferSize, const char* const _Name)
 // 전 많이 만들수록 좋아합니다.
 
 // 행동
-void CreatePlayer(const char* const _Ptr, int _Att, int _Hp)
+void CreatePlayer(const char* const _Ptr, const char* const _uiPtr, int _Att, int _Hp)
 {
     // char[100] = 300번지
     StrCopy(PlayerName, NAMELEN, _Ptr);
+    StrCopy(UIName, NAMELEN, _uiPtr);
     PlayerAtt = _Att;
     PlayerHp = _Hp;
 }
 
 // 모든 문법은 코드를 덜치기 위해서 발전해왔습니다.
-void CreateMonster(const char* const _Ptr, int _Att, int _Hp)
+void CreateMonster(const char* const _Ptr, const char* const _uiPtr, int _Att, int _Hp)
 {
     StrCopy(MonsterName, NAMELEN, _Ptr);
+    StrCopy(UIName, NAMELEN, _uiPtr);
     MonsterAtt = _Att;
     MonsterHp = _Hp;
 }
 
-void StatusRender(const char* _Name, int _Att, int _HP)
+void StatusRender(const char* _Name, const char* _UI, int _Att, int _HP)
 {
-    printf_s("%s Status", _Name);
-    int nameCount = strlen(_Name)+7;
-    for (int i = 0; i < LINECOUNT-nameCount; i += 1)
+    printf_s("%s %s", _Name ,_UI);
+
+    int nameCount = strlen(_Name);
+    int uiCount = strlen(_UI);
+    for (int i = 0; i < LINECOUNT-nameCount-uiCount-1; i += 1)
     {
         printf_s("-");
     }
@@ -80,12 +87,12 @@ void StatusRender(const char* _Name, int _Att, int _HP)
 
 void PlayerStatusRender()
 {
-    StatusRender(PlayerName, PlayerAtt, PlayerHp);
+    StatusRender(PlayerName,UIName, PlayerAtt, PlayerHp);
 }
 
 void MonsterStatusRender()
 {
-    StatusRender(MonsterName, MonsterAtt, MonsterHp);
+    StatusRender(MonsterName,UIName, MonsterAtt, MonsterHp);
 }
 
 int main()
@@ -94,8 +101,8 @@ int main()
     /*char Test1[50] = Test0;
     Test1 = Test0*/;
 
-    CreatePlayer("asdsadsadas", 10, 100);
-    CreateMonster("Orc", 10, 50);
+    CreatePlayer("Player","status", 10, 100);
+    CreateMonster("Orc","status", 10, 50);
 
     PlayerStatusRender();
     MonsterStatusRender();
