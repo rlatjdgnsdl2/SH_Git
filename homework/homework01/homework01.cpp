1b96e1badf713cfc1aafeaa1f157817ba200f770
rlatjdgnsdl2 <rlatjdgnsdl2@gmail.com>
2024-09-24 11:42:47 +0900

    PlusString

--- homework/homework01/homework01.cpp
+++ homework/homework01/homework01.cpp
@@ -1,23 +1,83 @@
 #include <iostream>
 using namespace std;
 
-
-
-
-
-
-
-
-int StringCount(const char* _str) 
+int StringCount(const char* _str)
 {
 	int count = 0;
 	while (_str[count])
 	{
 		count++;
-
 	}
 	return count;
+}
 
+int NumberCount(int _Value)
+{
+	// /(나누기를 최대한) 연산을 사용하세요.
+	// 
+	// 내가 한 버전
+   /* int Result = 0;
+	int DevisionNum = 1;
+	while (true)
+	{
+		if (_Value / DevisionNum==0)
+		{
+			break;
+		}
+		DevisionNum *= 10;
+		Result++;
+	}
+
+	return Result;*/
+
+	// 힌트주셔서 다시 푼것
+	int Result = 0;
+	while (true)
+	{
+		_Value /= 10;
+		Result++;
+		if (_Value == 0)
+		{
+			break;
+		}
+	}
+	return Result;
+}
+
+void PlusString(char* _Buffer, int _BufferSize, const char* const _Value0, const char* const _Value1)
+{
+	int count01 = StringCount(_Value0);
+	int count02 = StringCount(_Value1);
+
+	for (int i = 0; i < count01; i++) 
+	{
+		_Buffer[i] = _Value0[i];
+	}
+	for (int i = 0; i < count02; i++)
+	{
+		_Buffer[i+count01] = _Value1[i];
+	}
+
+
+}
+void NumberToString(char* Buffer, int BufferSize, int _Value)
+{
+	int count = NumberCount(_Value); //value 자릿수
+	if (count > BufferSize)
+	{
+		return;
+	}
+	for (int i = count - 1; i >= 0; i--)
+	{
+		//	3712 % 10 = 2 가 Buffer[3]에 들어가고 _value 371
+		//  371 % 10 = 1 가 Buffer[2]에 들어가고 _value 37
+		//  37 % 10 = 7 가 Buffer[1]에 들어가고 _value 3
+		//  3 % 10 = 3 가 Buffer[0]에 들어가고 _value 0
+		int result = _Value % 10;
+		Buffer[i] = result + '0';
+		_Value /= 10;
+
+	}
 }
 
 
@@ -27,7 +87,11 @@ int main()
 {
 	// [B][B][B][B][0]
 
-	int Count = StringCount("BBBB");
+	char Arr[100] = {};
+
+	PlusString(Arr, 100, "cccccccccccc", "BBBB");
+
+	std::cout << "Hello World!\n";
 
 	int a = 0;
 }
--- homework/homework2/homework02.cpp
+++ homework/homework2/homework02.cpp
@@ -5,86 +5,15 @@
 
 // 음수 안함
 // 0 이외의 양수의 자릿수를 세자.
-int NumberCount(int _Value)
-{
-	// /(나누기를 최대한) 연산을 사용하세요.
-	// 
-	// 내가 한 버전
-   /* int Result = 0;
-	int DevisionNum = 1;
-	while (true)
-	{
-		if (_Value / DevisionNum==0)
-		{
-			break;
-		}
-		DevisionNum *= 10;
-		Result++;
-	}
 
-	return Result;*/
-
-	// 힌트주셔서 다시 푼것
-	int Result = 0;
-	while (true)
-	{
-		_Value /= 10;
-		Result++;
-		if (_Value == 0)
-		{
-			break;
-		}
-
-	}
-
-	return Result;
-}
 
 // 해도 되고 안해도 됩니다.
 // 내 기준으로 지금
 // _Value 를 넣어주면 Buffer에 
 // 숫자를 문자열로 만드는 함수.
-void NumberToString(char* Buffer, int BufferSize, int _Value)
-{
-	int count = NumberCount(_Value); //value 자릿수
-	if (count > BufferSize)
-	{
-		return;
-	}
-	for (int i = count - 1; i >= 0; i--)
-	{
-		//	3712 % 10 = 2 가 Buffer[3]에 들어가고 _value 371
-		//  371 % 10 = 1 가 Buffer[2]에 들어가고 _value 37
-		//  37 % 10 = 7 가 Buffer[1]에 들어가고 _value 3
-		//  3 % 10 = 3 가 Buffer[0]에 들어가고 _value 0
-		
-		int result = _Value % 10;
-		Buffer[i] = result + '0';
-		_Value /= 10;
 
-	}
-
-	
-}
 
 int main()
 {
-	// 5
-	int Result0 = NumberCount(12358);
-	// 4
-	int Result1 = NumberCount(5258);
-	// 7
-	int Result2 = NumberCount(5258111);
-
-	// 0은 영
-
-	char Buffer1[100] = { 0 };
-
-	NumberToString(Buffer1, 100, 3712);
-	char Buffer2[100] = { 0 };
-	NumberToString(Buffer2, 100, 37123213);
-	// Buffer == "3712"
-
-	int a = 0;
 
 }
